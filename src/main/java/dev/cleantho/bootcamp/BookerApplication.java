80b5c3d9a482078cf0849f38f604633787b82e39
cleantho <cleantho@gmail.com>
2024-10-07 00:54:24 -0300

    CORS FIX FOR SWAGGER

--- src/main/java/dev/cleantho/bootcamp/BookerApplication.java
+++ src/main/java/dev/cleantho/bootcamp/BookerApplication.java
@@ -3,8 +3,11 @@ package dev.cleantho.bootcamp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 // CORS FIX FOR SWAGGER
-//@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 @SpringBootApplication
 public class BookerApplication {
 
